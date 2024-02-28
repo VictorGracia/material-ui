c819445aeb46f7f1ea94706ee4ce9e5b55086323
Victor Gracia <victorgracia2010@hotmail.com>
2024-02-28 11:46:24 -0500

    agrego nuevo boton innapsis

---
+++ packages/mui-material/src/ButtonInnapsis/index.d.ts
@@ -0,0 +1,5 @@
+export { default } from './ButtonInnapsis';
+export * from './ButtonInnapsis';
+
+export { default as buttonClasses } from './buttonInnapsisClasses';
+export * from './buttonInnapsisClasses';
---
+++ packages/mui-material/src/ButtonInnapsis/index.js
@@ -0,0 +1,5 @@
+'use client';
+export { default } from './ButtonInnapsis';
+
+export { default as buttonClasses } from './buttonInnapsisClasses';
+export * from './buttonInnapsisClasses';
